5d4196fe78063703f3a4cc98b434366e5fe1c517
Gerald Fehringer <gerald.fehringer@mc-ops.cloud>
2017-11-04 11:01:49 +0100

    Initial Upload

---
+++ ACS/ACS-ENGINE/setup-caas.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+#########
+FRESH="YES"                 ## If no, existing Caas will be removed!!                  
+BASE="/srv/ibm-caas2"
+ICP_RELEASE="2.1.0-beta-2"
+MASTER_IP="CHANGE-ME"       ## CE Edition doesnt allow HA-cluster
+PROXY_IP="CHANGE-ME"        ## Multiple Proxies can be used in CE Edition
+CLUSTER_DOMAIN="prodcluster.local"
+CLUSTER_NAME="caas01"
+COMPANY="newCO"
+PRODUCT="CaaS"
+SSH_KEY_EXIST="/root/.ssh/mycaasmgmtkey4096"  #use existing ssh key (will be used for ssh public-key auth!)
+DEBUG="FALSE"               # Set it to true if you are new to the IBM ICP setup
+ICP_HOSTS="/root/icp_hosts" #add a file with ICP hosts or leave it empty
+NFS="YES"
+#########
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+#
+
+# additonal packages we need
+apt-get -y update && apt-get -y upgrade && apt-get -y dist-upgrade
+apt-get -y install glusterfs-client python-pip nfs-common
+modprobe dm_thin_pool
+echo dm_thin_pool | tee -a /etc/modules
+
+
+if [ "${MASTER_IP}" == "CHANGE-ME" ]; then
+  echo ""
+  echo -e "$COL_RED ## PLEASE, adjust base vars virst !! ## $COL_RESET"
+  exit 1
+fi
+
+if [ "${FRESH}" == "YES" ]; then
+### pre-req
+if [ ! -d "${BASE}" ]; then
+  mkdir -p ${BASE}
+fi
+
+if [ "$BFS" == "YES" ]; then
+  echo ""
+  echo -e "$COL_RED ## NFS Installation ## $COL_RESET"
+  apt-get -y install nfs-kernel-server
+  mkdir /var/nfs/general -p
+  chown nobody:nogroup /var/nfs/general
+  echo "
+/var/nfs/general    127.0.0.1(rw,sync,no_subtree_check)
+/var/nfs/general    $MASTER_IP(rw,sync,no_subtree_check)
+" >/etc/exports
+  systemctl restart nfs-kernel-server
+fi
+#
+
+if [ ! -f "$SSH_KEY_EXIST" ]; then
+   ssh-keygen -t rsa -b 4096 -N "" -f /root/.ssh/mycaasmgmtkey4096
+   cat /root/.ssh/mycaasmgmtkey4096.pub >> ~/.ssh/authorized_keys
+fi
+
+
+### IBM ICP CE Install
+echo -e "$COL_MAGENTA ## Pull initial IBM ICP Installer Docker image.. $COL_RESET"
+
+docker pull ibmcom/icp-inception:${ICP_RELEASE}
+cd ${BASE}
+docker run -e LICENSE=accept \
+-v "$(pwd)":/data ibmcom/icp-inception:${ICP_RELEASE} cp -r cluster /data
+
+echo -e "$COL_MAGENTA ## Adjust necessary config in ${BASE}/cluster directory... $COL_RESET"
+### configuration
+cd ${BASE}/cluster
+cp config.yaml config.yaml.ORIG
+if [ -f "$SSH_KEY_EXIST" ]; then
+   cp "$SSH_KEY_EXIST" ssh_key
+else
+  cp /root/.ssh/caasid_rsa4096 ssh_key
+fi
+
+#
+if [ ! -z "$ICP_HOSTS" ]; then
+  cp "$ICP_HOSTS" ${BASE}/cluster/hosts
+  echo -e "$COL_YELLOW ## Don't forget to copy the new public RSA4096-ssh-key to all nodes in authorized_keys!! $COL_RESET"
+  sleep 5
+fi
+#
+if [ -z "$ICP_HOSTS" ]; then
+echo "[master]
+${MASTER_IP}
+
+[worker]
+${MASTER_IP}
+
+[proxy]
+${PROXY_IP}
+" >hosts
+fi
+#
+echo "network_cidr: 10.1.0.0/16
+service_cluster_ip_range: 10.0.0.1/24
+#
+default_admin_user: admin
+default_admin_password: SecAdminChange
+#
+secure_connection_enabled: true
+#
+cluster_domain: ${CLUSTER_DOMAIN}
+cluster_name: ${CLUSTER_NAME}
+#
+#metering_enabled: true
+#
+auditlog_enabled: true
+" >config.yaml
+#
+if [ "${DEBUG}" == "TRUE" ]; then
+ printf "\n### OKAY. please verify all ICP configs and than proceed manual!\n"
+ exit 1 
+else
+ echo -e "$COL_MAGENTA ## NOW, we will start the installer, depending on you node, it will take at least 10min... $COL_RESET"
+ echo -e "$COL_MAGENTA ## YOU HAVE 10sec to interrupt now :-> $COL_RESET"
+ sleep 10
+ docker run -e LICENSE=accept --net=host -t -v "$(pwd)":/installer/cluster \
+ ibmcom/icp-inception:${ICP_RELEASE} install
+ fi
+
+ echo -e "$COL_YELLOW ## Be aware, the are a lot of docker images to be taken care of, so wait 10 more mins, before logging in to the web ui... $COL_RESET"
+ echo -e "$COL_RED ## Change Default Credentials!! admin / SecAdminChange $COL_RESET"
+ sleep 4
+fi
+
+
+### addons
+echo -e "$COL_MAGENTA ## Installing necessary addons, like HELM, ISTIO, Kubectl... $COL_RESET"
+cd /tmp
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
+mv kubectl /usr/local/bin/
+#
+cd /tmp
+curl -L https://git.io/getIstio | sh -
+chmod +x istio-*/bin/istioctl && cp -p istio-*/bin/istioctl /usr/local/bin
+#
+echo 'kubectl get pods' >>/root/.bashrc
+#
+
+### customize ibm main http container
+#docker inspect `docker ps |grep "icp-router" |grep -v "elastics" |cut -d" " -f1`
+echo -e "$COL_MAGENTA ## Customizing IBM ICP Branding in the web-ui... $COL_RESET"
+for i in `find /var/lib/docker -name "index.[0-9]*.js"`
+do
+  #adjust javascript
+  sed -i "s/IBM/${COMPANY}/g" $i
+  sed -i "s/private-ce/${PRODUCT}/g" $i
+  #adjust main index.html
+  index="$(echo ${i} | cut -d"/" -f1-13)"
+  sed -i "s/IBM/${COMPANY}/g" $index/index.html
+  sed -i "s/private-ce/${PRODUCT}/g" $index/index.html
+  sed -i "s/private//g" $index/index.html
+done
+
---
+++ ACS/ACS-ENGINE/setup.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+########## BASE VARS
+IP="CHANGE-ME"
+HOST="CHANGE-ME"
+RVM="2.4.0"
+TERRAFORM="0.10.7"
+###########
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+#
+
+if [ "${HOST}" == "CHANGE-ME" ]; then
+  echo ""
+  echo -e "$COL_RED ## PLEASE, adjust base vars virst !! ## $COL_RESET"
+  exit 1
+fi
+
+
+### base ubuntu setup
+#echo "${IP}  ${HOST}" >>/etc/hosts  ## FIX THIS!
+echo "${HOST}" >/etc/hostname
+#
+timedatectl set-ntp no
+#
+apt-get -y update && apt-get upgrade && apt-get -y dist-upgrade
+apt-get -y install build-essential apt-transport-https \
+ca-certificates curl software-properties-common \
+libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 \
+libsqlite3-dev libpcap-dev git-core autoconf curl zlib1g-dev \
+libxml2-dev libxslt1-dev libyaml-dev unzip zip ntp lsof ctop
+#
+if [ ! -f "/usr/bin/python" ]; then
+  ln -s /usr/bin/python3 /usr/bin/python
+fi
+#
+# azure cli repo
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+tee /etc/apt/sources.list.d/azure-cli.list
+apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+#
+echo "vm.max_map_count = 262144" >> /etc/sysctl.conf
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable"
+apt-get -y update
+# docker
+apt-get -y install docker-ce azure-cli
+echo '
+{
+  "storage-driver": "devicemapper"
+}
+' >/etc/docker/daemon.json
+#
+echo '#!/bin/bash
+#
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+#
+printf "_________________________________\n"
+echo -e "$COL_YELLOW * newCO CaaS Platform $COL_RESET"
+  if [ -f "/opt/ibm/cfc/version" ]; then
+    ICPVERSION=$(cat /opt/ibm/cfc/version)
+    echo -e "   Release:$COL_GREEN $ICPVERSION $COL_RESET"
+  fi
+echo ""
+#
+if [ -b "/dev/md0" ]; then
+  printf " * Local Disk Usage: \n"
+  df --output=size,used,avail,pcent,fstype,target -kh /dev/md* |grep -v "devtmpfs"
+  printf "\n"
+elif [ ! -z "/dev/mapper/$(hostname)*root*" ]; then
+  printf " * Local Disk Usage: \n"
+  pvs
+  printf "\n"
+fi
+#
+if [ -x /usr/bin/docker ]; then
+  TOTAL=$(docker ps --filter status=running |wc -l)
+  DCRELEASE=$(docker --version |cut -d" " -f3 |sed 's/,//g')
+  printf " * Docker Infos:\n"
+  printf " > Release: \t\t%13s \n" $DCRELEASE
+  printf " > Total Running Containers: \t%s \n" $TOTAL
+fi
+printf "\n"
+#' >/etc/update-motd.d/10-help-text
+
+#
+ssh-keygen -t rsa -b 4096 -N "" -f /root/.ssh/id_rsa4096
+cat /root/.ssh/id_rsa4096.pub >> ~/.ssh/authorized_keys
+##
+
+### terraform
+cd /tmp
+wget -q https://releases.hashicorp.com/terraform/${TERRAFORM}/terraform_${TERRAFORM}_linux_amd64.zip
+unzip terraform_${TERRAFORM}_linux_amd64.zip
+mv terraform /usr/local/bin/
+##
+
+### fix /tmp issues with later caas install
+echo "*/30 * * * *    chown root:root /tmp && chmod 1777 /tmp" |crontab -
+
+## latest rvm
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://get.rvm.io | bash -s stable --rails
+echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
+source ~/.bashrc
+rvm install ${RVM}
+rvm use ${RVM} --default
+
+##
+echo ""
+echo -e "$COL_YELLOW ## SEEMS we are done :-> ## $COL_RESET"
+echo -e "$COL_GREEN ## PLEASE, reboot your server now and afterwards start setup-caas.sh ## $COL_RESET"
+echo ""
+# EOF
+
